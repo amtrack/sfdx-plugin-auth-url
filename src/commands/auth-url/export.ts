1697a68cb96437ad364f160cbbe4345d2e702013
Matthias Rolke <mr.amtrack@gmail.com>
2022-10-23 14:18:57 +0200

    fix(deps): update dependencies and project

--- src/commands/auth-url/export.ts
+++ src/commands/auth-url/export.ts
@@ -1,21 +1,15 @@
-import { core, SfdxCommand } from '@salesforce/command';
+import { SfdxCommand } from '@salesforce/command';
 import type { AnyJson } from '@salesforce/ts-types';
 import { promisify } from 'util';
 import child_process = require('child_process');
 const exec = promisify(child_process.exec);
 
-// Initialize Messages with the current plugin directory
-core.Messages.importMessagesDirectory(__dirname);
-
-// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
-// or any library that is using the messages framework can also be loaded this way.
-const messages = core.Messages.loadMessages('sfdx-plugin-auth-url', 'export');
-
 export default class ExportOrgUsingAuthUrlCommand extends SfdxCommand {
-  public static description = messages.getMessage('commandDescription');
+  public static description =
+    'Print a Sfdx Auth Url.\nThis is a wrapper for sfdx force:org:display --verbose.';
 
   public static examples = [
-    `$ sfdx auth-url:export --targetusername myOrg@example.com
+    `$ sfdx <%= command.id %> --targetusername myOrg@example.com
   force://PlatformCLI::xxx@amazing-cloudy-374844.my.salesforce.com
   `
   ];
