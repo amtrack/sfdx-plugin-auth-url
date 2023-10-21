d01a9437ec0cc2dd37e24db4dcc1425c1ee75a03
Matthias Rolke <mr.amtrack@gmail.com>
2023-10-21 22:27:04 +0200

    feat: sf style

--- src/commands/auth-url/export.ts
+++ src/commands/auth-url/export.ts
@@ -1,34 +1,35 @@
-import { SfdxCommand } from '@salesforce/command';
-import type { AnyJson } from '@salesforce/ts-types';
+import { SfCommand, requiredOrgFlagWithDeprecations } from '@salesforce/sf-plugins-core';
 import { promisify } from 'util';
 import child_process = require('child_process');
 const exec = promisify(child_process.exec);
 
-export default class ExportOrgUsingAuthUrlCommand extends SfdxCommand {
+export type ExportOrgUsingAuthUrlResult = {
+  sfdxAuthUrl: string;
+};
+
+export class ExportOrgUsingAuthUrlCommand extends SfCommand<ExportOrgUsingAuthUrlResult> {
   public static description =
     'Print a Sfdx Auth Url.\nThis is a wrapper for sfdx force:org:display --verbose.';
 
   public static examples = [
-    `$ sfdx <%= command.id %> --targetusername myOrg@example.com
+    `$ <%= config.bin %> <%= command.id %> --targetusername myOrg@example.com
   force://PlatformCLI::xxx@amazing-cloudy-374844.my.salesforce.com
   `
   ];
 
-  public static args = [];
+  public static readonly flags = {
+    "target-org": requiredOrgFlagWithDeprecations,
+  }
 
-  protected static flagsConfig = {};
-
-  // Comment this out if your command does not require an org username
-  protected static requiresUsername = true;
-
-  public async run(): Promise<AnyJson> {
+  public async run(): Promise<ExportOrgUsingAuthUrlResult> {
+    const { flags } = await this.parse(ExportOrgUsingAuthUrlCommand);
     const args = ['--verbose', '--json'];
-    if (this.flags.targetusername) {
-      args.push(...['--targetusername', this.flags.targetusername]);
+    if (flags['target-org']) {
+      args.push(...['--targetusername', flags["target-org"].getUsername()!]);
     }
     const execResult = await exec(`sfdx force:org:display ${args.join(' ')}`);
-    const stdoutJson = JSON.parse(execResult.stdout);
-    this.ux.log(stdoutJson.result.sfdxAuthUrl);
-    return { sfdxAuthUrl: stdoutJson.result.sfdxAuthUrl };
+    const { result } = JSON.parse(execResult.stdout);
+    this.log(result.sfdxAuthUrl);
+    return { sfdxAuthUrl: result.sfdxAuthUrl };
   }
 }
